268b5aacb3fa61f50a363d7c627ed56b71bea0d7
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-09-04 11:57:06 +0100

    Update bike model

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -2,5 +2,5 @@ class Bike < ApplicationRecord
   has_many :appointments, dependent: :destroy
   has_many :users, through: :appointments
 
-  validates :model, :color, :price, :weight, :engine_capacity, presence: true, allow_nil: false, allow_blank: false
+  validates_presence_of :model, :color, :price, :weight, :engine_capacity
 end
